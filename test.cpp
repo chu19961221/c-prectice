3dbd3f92e95514f5b27a394e74765df9a76d9ce5
chu19961221 <chu19961221@gmail.com>
2018-08-09 09:47:08 +0800

    v1

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<cstdlib>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    float b=5.5;
+    int x=a+b;
+    cout<<x<<endl;
+    system("pause");
+    return 0;
+}
